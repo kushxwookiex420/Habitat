3f38f5e7a70664e600fef6e8d2d59c9612f69f25
kushxwookiex420 <kushxwookiex420@gmail.com>
2026-09-24 17:58:00 -0500

    Habitat 0.3.4: replace flat UI with interactive 3D brain interface

--- app/src/main/java/com/habitat/MainActivity.kt
+++ app/src/main/java/com/habitat/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.FrameLayout
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,12 @@ class MainActivity : Activity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.navigationBarColor = android.graphics.Color.BLACK
-        setContentView(GalaxyHabitatView(this))
+        val root = FrameLayout(this)
+        val space = Habitat3DSurface(this)
+        val ui = HabitatOverlay(this)
+        root.addView(space, FrameLayout.LayoutParams(-1,-1))
+        root.addView(ui, FrameLayout.LayoutParams(-1,-1))
+        ui.onSectionChanged = { space.setMode(it) }
+        setContentView(root)
     }
 }
