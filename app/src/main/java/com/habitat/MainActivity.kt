1d06fd4a1839fda15672c40b4917a6378e3f4780
kushxwookiex420 <kushxwookiex420@gmail.com>
2026-09-24 09:15:58 -0500

    Habitat v0.3.3: build core and Galaxy Brain UI

---
+++ app/src/main/java/com/habitat/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.habitat
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.navigationBarColor = android.graphics.Color.BLACK
+        setContentView(GalaxyHabitatView(this))
+    }
+}
