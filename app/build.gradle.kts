984bb61ea4d797f2eae4c5bbf3c3baa8864bf441
kushxwookiex420 <kushxwookiex420@gmail.com>
2026-09-24 09:19:41 -0500

    Habitat v0.3.3: align Java and Kotlin JVM targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,4 +12,14 @@ android {
         versionCode = 33
         versionName = "0.3.3"
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+kotlin {
+    jvmToolchain(17)
 }
